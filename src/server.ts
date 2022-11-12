d5a2b1a62b8bd1fd0642d2b93f74f4f8444d4735
André Luiz Perensin <andreluizperensin@gmail.com>
2022-11-11 22:20:47 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+
+import { authRoutes } from "./routes/Auth";
+import { userRoutes } from "./routes/User";
+import { poolRoutes } from "./routes/Pool";
+import { guessRoutes } from "./routes/Guess";
+import { gameRoutes } from "./routes/Game";
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  });
+
+  await fastify.register(cors, {
+    origin: true,
+  });
+
+  await fastify.register(jwt, {
+    secret: process.env.JWT_SECRET!,
+  });
+
+  await fastify.register(authRoutes);
+  await fastify.register(userRoutes);
+  await fastify.register(poolRoutes);
+  await fastify.register(guessRoutes);
+  await fastify.register(gameRoutes);
+
+  await fastify.listen({ port: Number(process.env.PORT!) });
+  // await fastify.listen({ port: 3333, host: "192.168.50.104" });
+}
+
+bootstrap();
