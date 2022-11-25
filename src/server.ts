af4a379b997866b9d04da1364209a469168ce9af
André Luiz Perensin <andreluizperensin@gmail.com>
2022-11-24 22:37:16 -0300

    add host to server config

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,10 @@ async function bootstrap() {
   await fastify.register(guessRoutes);
   await fastify.register(gameRoutes);
 
-  await fastify.listen({ port: Number(process.env.PORT) || 3000 });
+  await fastify.listen({
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 3000,
+  });
   // await fastify.listen({ port: 3333, host: "192.168.50.104" });
 }
 
